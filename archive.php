175008ec8c6104995a7aabe838795e2a5e14432f
Edward Caissie <edward.caissie@gmail.com>
2012-04-13 16:13:11 -0400

    Update, optimize, and standardize code structure

--- archive.php
+++ archive.php
@@ -1,37 +1,53 @@
-<?php get_header(); ?>
+<?php
+/**
+ * Archive Template
+ *
+ * Displays archives page is required to be displayed
+ *
+ * @package     GroundFloor
+ * @since       1.0
+ *
+ * @link        http://buynowshop.com/themes/ground-floor/
+ * @link        https://github.com/Cais/ground-floor/
+ * @link        http://wordpress.org/extend/themes/ground-floor/
+ *
+ * @author      Edward Caissie <edward.caissie@gmail.com>
+ * @copyright   Copyright (c) 2009-2012, Edward Caissie
+ */
+
+get_header(); ?>
 <div id="main-blog">
-  <div id="content">
-    <?php if ( have_posts() ) : ?>
-      <?php while ( have_posts() ) : the_post(); ?>
-	<div <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-	  <h2><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php _e( 'Permanent Link to', 'groundfloor' ); ?> <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-	  <div class="post-details">
-	    <?php _e( 'Posted by ', 'groundfloor' ); the_author(); _e( ' on ', 'groundfloor' ); the_time( get_option( 'date_format' ) ); ?>
-	    <?php _e( 'with ', 'groundfloor' ); comments_popup_link( __( 'No Comments', 'groundfloor' ), __( '1 Comment', 'groundfloor' ), __( '% Comments', 'groundfloor' ), '', __( 'Comments Closed', 'groundfloor' ) ); ?>
-	    <?php edit_post_link( __( 'Edit', 'groundfloor' ), __( '&#124; ', 'groundfloor' ), __( '', 'groundfloor' ) ); ?><br />
-	    <?php _e( ' in ', 'groundfloor' );?><?php the_category( ', ' ) ?><br />
-	    <?php the_tags( __( 'as ', 'groundfloor' ), ', ', '' ); ?><br />
-	  </div> <!-- .post-details -->
-	  <?php the_excerpt(); ?>
-	  <div class="clear"></div> <!-- For inserted media at the end of the post -->
-	</div> <!-- post_class -->
-      <?php endwhile; ?>
-      <div id="nav-global" class="navigation">
-	<div class="left">
-	  <?php next_posts_link( __( '&laquo; Previous entries ', 'groundfloor' ) ); ?>
-	</div>
-	<div class="right">
-	  <?php previous_posts_link( __( ' Next entries &raquo;', 'groundfloor' ) ); ?>
-	</div>
-      </div> <!-- .navigation -->
-      <div class="clear"></div>
-    <?php else : ?>
-      <h2><?php printf( __( 'Search Results for: %s', 'groundfloor' ), '<span>' . esc_html( get_search_query() ) . '</span>' ); ?></h2>
-      <p><?php _e( 'Sorry, but you are looking for something that is not here.', 'groundfloor' ); ?></p>
-      <?php get_search_form(); ?>
-    <?php endif; ?>
-  </div> <!-- #content -->
+    <div id="content">
+        <?php if ( have_posts() ) :
+            while ( have_posts() ) : the_post(); ?>
+                <div <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+                    <h2><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php _e( 'Permanent Link to', 'groundfloor' ); ?> <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+                    <div class="post-details">
+                        <?php _e( 'Posted by ', 'groundfloor' ); the_author(); _e( ' on ', 'groundfloor' ); the_time( get_option( 'date_format' ) );
+                        _e( 'with ', 'groundfloor' ); comments_popup_link( __( 'No Comments', 'groundfloor' ), __( '1 Comment', 'groundfloor' ), __( '% Comments', 'groundfloor' ), '', __( 'Comments Closed', 'groundfloor' ) );
+                        edit_post_link( __( 'Edit', 'groundfloor' ), __( '&#124; ', 'groundfloor' ), __( '', 'groundfloor' ) );
+                        _e( '<br />in ', 'groundfloor' );?><?php the_category( ', ' ) ?><br />
+                        <?php the_tags( __( 'as ', 'groundfloor' ), ', ', '' ); ?><br />
+                    </div> <!-- .post-details -->
+                    <?php the_excerpt(); ?>
+                    <div class="clear"></div> <!-- For inserted media at the end of the post -->
+                </div> <!-- post_class -->
+            <?php endwhile; ?>
+            <div id="nav-global" class="navigation">
+                <div class="left">
+                    <?php next_posts_link( __( '&laquo; Previous entries ', 'groundfloor' ) ); ?>
+                </div>
+                <div class="right">
+                    <?php previous_posts_link( __( ' Next entries &raquo;', 'groundfloor' ) ); ?>
+                </div>
+            </div> <!-- .navigation -->
+            <div class="clear"></div>
+        <?php else : ?>
+            <h2><?php printf( __( 'Search Results for: %s', 'groundfloor' ), '<span>' . esc_html( get_search_query() ) . '</span>' ); ?></h2>
+            <p><?php _e( 'Sorry, but you are looking for something that is not here.', 'groundfloor' ); ?></p>
+            <?php get_search_form();
+        endif; ?>
+    </div> <!-- #content -->
 </div> <!-- #main-blog -->
-<?php get_sidebar(); ?>
-<?php get_footer();?>
-<?php /* Last Revision: October 2, 2010 v1.6 */ ?>
+<?php get_sidebar();
+get_footer();?>
