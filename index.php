8dccc8442b91adab014278faf62fd698aac325d0
Edward Caissie <edward.caissie@gmail.com>
2012-04-19 11:39:27 -0400

    Update to version 1.9.1 ... wrong archive for 1.9 uploaded

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
  * @internal    REQUIRES WordPress version 3.0
  * @internal    Tested up to WordPress version 3.4
  *
- * @version     1.9
+ * @version     1.9.1
  * @author      Edward Caissie <edward.caissie@gmail.com>
  * @copyright   Copyright (c) 2009-2012, Edward Caissie
  *
