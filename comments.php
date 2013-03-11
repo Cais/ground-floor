3d8dd9efea12b24659baf52c39570f9538e71879
Edward Caissie <edward.caissie@gmail.com>
2013-03-11 16:07:29 -0400

    * Refactored code formatting and code block termination comments * Refactored post meta to be more i18n compatible

--- comments.php
+++ comments.php
@@ -10,17 +10,23 @@
  * @link        http://wordpress.org/extend/themes/ground-floor/
  *
  * @author      Edward Caissie <edward.caissie@gmail.com>
- * @copyright   Copyright (c) 2009-2012, Edward Caissie
+ * @copyright   Copyright (c) 2009-2013, Edward Caissie
+ *
+ * @version     2.2
+ * @date        March 11, 2013
+ * Refactored code formatting and code block termination comments
+ * Refactored post meta to be more i18n compatible
  */
 
 /** Do not delete these lines */
-if ( ! empty( $_SERVER['SCRIPT_FILENAME'] ) && 'comments.php' == basename( $_SERVER['SCRIPT_FILENAME'] ) )
-	die ( __( 'Please do not load this page directly. Thanks!', 'groundfloor' ) );
+if ( ! empty( $_SERVER['SCRIPT_FILENAME'] ) && 'comments.php' == basename( $_SERVER['SCRIPT_FILENAME'] ) ) {
+    die ( __( 'Please do not load this page directly. Thanks!', 'groundfloor' ) );
+} /** End if - not empty */
 if ( post_password_required() ) { ?>
 	<p class="nocomments"><?php _e( 'This post is password protected. Enter the password to view comments.', 'groundfloor' ); ?></p>
 <?php
 	return;
-}
+} /** End if - post password required */
 
 /**
  * Comment Add Micro ID
@@ -32,14 +38,17 @@ if ( post_password_required() ) { ?>
  * @return array
  */
 function comment_add_microid( $classes ) {
+
     $c_email = get_comment_author_email();
     $c_url = get_comment_author_url();
     if ( ! empty( $c_email ) && ! empty( $c_url ) ) {
         $microid = 'microid-mailto+http:sha1:' . sha1( sha1( 'mailto:' . $c_email ) . sha1( $c_url ) );
         $classes[] = $microid;
-    }
+    } /** End if - not empty */
+
     return $classes;
-}
+
+} /** End function - add micro ID */
 add_filter( 'comment_class', 'comment_add_microid' );
 
 /**
@@ -52,35 +61,49 @@ add_filter( 'comment_class', 'comment_add_microid' );
  * @return array
  */
 function comment_add_userid( $classes ) {
+
     global $comment;
     if ( $comment->user_id == 1 ) { /** Default Administrator Account */
         $userid = "administrator user-id-1";
     } else { /** All other users - NB: user-id-0 -> non-registered user */
         $userid = "user-id-" . ( $comment->user_id );
-    }
+    } /** End if - comment user ID is 1 */
     $classes[] = $userid;
+
     return $classes;
-}
-add_filter( 'comment_class', 'comment_add_userid' );
-?>
+
+} /** End function - add user ID */
+add_filter( 'comment_class', 'comment_add_userid' ); ?>
 
 <div id="comments-main">
+
     <?php /** show the comments */
-    if ( have_comments() ) : ?>
-        <h4 id="comments"><?php comments_number( __( 'No Comments', 'groundfloor' ), __( 'One Comment', 'groundfloor' ), __( '% Comments', 'groundfloor' ) );?></h4>
+    if ( have_comments() ) { ?>
+
+        <h4 id="comments">
+            <?php comments_number( __( 'No Comments', 'groundfloor' ), __( 'One Comment', 'groundfloor' ), __( '% Comments', 'groundfloor' ) );?>
+        </h4><!-- #comments -->
+
         <ul class="commentlist" id="singlecomments">
             <?php wp_list_comments( array( 'avatar_size'=>60, 'reply_text'=>__( '&raquo; Reply to this Comment &laquo;', 'groundfloor' ) ) ); ?>
-        </ul>
+        </ul><!-- commentlist -->
+
         <div class="navigation">
             <div class="alignleft"><?php previous_comments_link() ?></div>
             <div class="alignright"><?php next_comments_link() ?></div>
         </div><!-- .navigation -->
-    <?php else : /** this is displayed if there are no comments so far */
-        if ( 'open' == $post->comment_status ) :
+
+    <?php } else {
+
+        /** this is displayed if there are no comments so far */
+        if ( 'open' == $post->comment_status ) {
             /** If comments are open, but there are no comments. */
-        else :
+        } else {
             /** comments are closed */
-        endif;
-    endif;
+        } /** End if - comments open */
+
+    } /** End if - have comments */
+
     comment_form(); ?>
+
 </div><!-- #comments-main -->
