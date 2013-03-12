4af59f0e8d4cb9be03e9d215f3da3587d0c7f590
Edward Caissie <edward.caissie@gmail.com>
2013-03-12 09:12:53 -0400

    * Refactored code formatting and code block termination comments

--- page.php
+++ page.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Footer Template
+ * Page Template
  *
  * @package     GroundFloor
  * @since       1.0
@@ -10,39 +10,71 @@
  * @link        http://wordpress.org/extend/themes/ground-floor/
  *
  * @author      Edward Caissie <edward.caissie@gmail.com>
- * @copyright   Copyright (c) 2009-2012, Edward Caissie
+ * @copyright   Copyright (c) 2009-2013, Edward Caissie
+ *
+ * @version 2.2
+ * @date    March 11, 2013
+ * Refactored code formatting and code block termination comments
  */
 
 get_header(); ?>
+
 <div id="main-blog">
+
     <div id="content">
+
         <?php
-        if ( have_posts() ) :
-            while ( have_posts() ) : the_post(); ?>
+        if ( have_posts() ) {
+            while ( have_posts() ) {
+                the_post(); ?>
                 <div id="page-content">
+
                     <div <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-                        <h1><?php the_title(); ?></h1>
+
+                        <h1>
+                            <?php the_title(); ?>
+                        </h1>
+
                         <div id="page-meta">
                             <?php comments_popup_link( __( 'with No Comments', 'groundfloor' ), __( 'with 1 Comment', 'groundfloor' ), __( 'with % Comments', 'groundfloor' ), '', __( '', 'groundfloor' ) ); ?>
                             <?php edit_post_link( __( 'Edit this page', 'groundfloor' ), __( '&gt ', 'groundfloor' ), __( '', 'groundfloor' ) ); ?>
                         </div><!-- #page-meta -->
+
                         <?php
                         if ( has_post_thumbnail() ) {
                             the_post_thumbnail( 'thumbnail', array( 'class' => 'alignleft' ) );
-                        }
+                        } /** End if - has post thumbnails */
+
                         the_content( __( 'Read more ...', 'groundfloor' ) );
+
                         wp_link_pages( array( 'before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number' ) ); ?>
+
                         <div class="clear"></div><!-- For inserted media at the end of the post -->
+
                     </div><!-- post_class -->
+
                     <?php comments_template(); ?>
+
                 </div><!-- #page-content -->
-            <?php endwhile;
-        else : ?>
-            <h2><?php printf( __( 'Search Results for: %s', 'groundfloor' ), '<span>' . esc_html( get_search_query() ) . '</span>' ); ?></h2>
+
+            <?php } /** End while - have posts */
+
+        } else { ?>
+
+            <h2>
+                <?php printf( __( 'Search Results for: %s', 'groundfloor' ), '<span>' . esc_html( get_search_query() ) . '</span>' ); ?>
+            </h2>
             <p><?php _e( 'Sorry, but you are looking for something that is not here.', 'groundfloor' ); ?></p>
-            <?php get_search_form();
-        endif; ?>
+
+            <?php
+            get_search_form();
+
+        } /** end if - have posts */ ?>
+
     </div><!-- #content -->
+
 </div><!-- #main-blog -->
-<?php get_sidebar();
+
+<?php
+get_sidebar();
 get_footer();
