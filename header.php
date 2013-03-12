fafc04a0a967e6a100808023cb7f4183a537ad61
Edward Caissie <edward.caissie@gmail.com>
2013-03-12 09:08:22 -0400

    * Updated DOCTYPE and other related header elements

--- header.php
+++ header.php
@@ -1,5 +1,3 @@
-<!DOCTYPE PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
 <?php
 /**
  * Header
@@ -14,35 +12,60 @@
  * @link        http://wordpress.org/extend/themes/ground-floor/
  *
  * @author      Edward Caissie <edward.caissie@gmail.com>
- * @copyright   Copyright (c) 2009-2012, Edward Caissie
+ * @copyright   Copyright (c) 2009-2013, Edward Caissie
  *
  * @version     2.0
  * @date        July 5, 2012
  * Updated HTML title tag
- */
-?>
-<head profile="http://gmpg.org/xfn/11">
-    <meta http-equiv="content-type" content="<?php bloginfo( 'html_type' ); ?>; charset=<?php bloginfo( 'charset' ); ?>" />
+ *
+ * @version     2.1
+ * @date        March 12, 2013
+ * Updated DOCTYPE and other related header elements
+ * Code formatting to be more easily read
+ */ ?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <title><?php wp_title( '|', true, 'right' ); ?></title>
+    <link rel="profile" href="http://gmpg.org/xfn/11" />
     <link rel="stylesheet" type="text/css" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
     <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<div id="outside">
+
+<div id="outside"><!-- ends in footer.php -->
+
     <div id="header">
+
         <div id="header-top"></div>
+
         <div id="header-middle">
+
             <div id="header-title">
-                <div id="blog-title"><span><a href="<?php echo home_url( '/' ); ?>" title="<?php bloginfo( 'name' ) ?>" rel="home"><?php bloginfo( 'name' ) ?></a></span></div>
-                <div id="blog-description"><?php bloginfo( 'description' ) ?></div>
+
+                <div id="blog-title">
+                    <span><a href="<?php echo home_url( '/' ); ?>" title="<?php bloginfo( 'name' ) ?>" rel="home"><?php bloginfo( 'name' ) ?></a></span>
+                </div><!-- #blog-title -->
+
+                <div id="blog-description">
+                    <?php bloginfo( 'description' ) ?>
+                </div><!-- #blog-description -->
+
             </div><!-- #header-title -->
+
             <div id="top-navigation-menu">
                 <?php gf_nav_menu(); ?>
             </div><!-- #top-navigation-menu -->
+
             <div class="clear"></div>
+
         </div><!-- #header-middle -->
+
         <div id="header-bottom"></div>
+
     </div><!-- #header -->
-    <div id="head2toe">
+
+    <div id="head2toe"><!-- ends in footer.php -->
